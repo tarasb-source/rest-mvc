183ea8e8545e2665ad1829df2e3cec11dbf505e7
tarasb-source” <taras.brytskiy@gmail.com>
2026-06-03 13:17:03 -0400

    Changed Category to be annotated with Entity

--- src/main/java/tarasb/springframework/domain/Category.java
+++ src/main/java/tarasb/springframework/domain/Category.java
@@ -1,11 +1,13 @@
 package tarasb.springframework.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Category {
 
     @Id
